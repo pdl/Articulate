c86b3d1073183fed96df0cfc480aba5a60ef2eb1
Daniel Perrett <perrettdl@googlemail.com>
2014-12-16 08:45:05 +0000

    Documented Location

--- lib/Articulate/Location.pm
+++ lib/Articulate/Location.pm
@@ -4,6 +4,29 @@ use Scalar::Util qw(blessed);
 use overload  '""' => \&to_file_path, '@{}' => sub{ shift->path };
 use Dancer::Plugin ();
 
+=head1 NAME
+
+Articulate::Location - represent an item's location
+
+=cut
+
+=head1 DESCRIPTION
+
+  loc ['zone', 'public', 'article', 'hello-world']
+  loc 'zone/public/article/hello-world' # same thing
+
+An object class which represents an item's location within the Articulate ecosystem. It contains an array of slugs, and stringifies to the 'file path' representation of them.
+
+=cut
+
+=head1 FUNCTIONS
+
+=head3 loc
+
+C<loc> is a constructor. It takes either a string (in the form of a path) or an arrayref. Either will be stored as an arrayref in the C<path> attribute.
+
+=cut
+
 Dancer::Plugin::register loc => sub {
   if ( 1 == scalar @_ ) {
     if ( blessed $_[0] and $_[0]->isa('Articulate::Location') ) {
@@ -16,7 +39,7 @@ Dancer::Plugin::register loc => sub {
       return __PACKAGE__->new;
     }
     elsif ( !ref $_[0] ) {
-      return __PACKAGE__->new({ path => [ split /\//, $_[0] ] });
+      return __PACKAGE__->new({ path => [ map { $_ ne '' } split /\//, $_[0] ] });
     }
     elsif ( ref $_[0] eq 'HASH' ) {
       return __PACKAGE__->new($_[0]);
@@ -24,14 +47,38 @@ Dancer::Plugin::register loc => sub {
   }
 };
 
+=head1 METHODS
+
+=head3 path
+
+An arrayref representing the path to the location. This is used for overloaded array dereferencing.
+
+=cut
+
 has path => (
   is      => 'rw',
   default => sub { []; },
 );
 
+=head3 location
+
+  $location->location->location # same as $location
+
+This method always returns the object itself.
+
+=cut
+
+
 sub location {
   return shift;
 }
+
+=head3 to_file_path
+
+Joins the contents of C<path> on C</> and returns the result. This is used for overloaded stringification.
+
+=cut
+
 sub to_file_path {
   return join '/', @{ $_[0]->path }
 };
