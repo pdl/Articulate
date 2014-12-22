054fcad24b566b76f0f8b7cb4b6bba45c35666f1
Daniel Perrett <perrettdl@googlemail.com>
2014-12-22 20:33:12 +0000

    Documented Request and Response

--- lib/Articulate/Request.pm
+++ lib/Articulate/Request.pm
@@ -4,6 +4,22 @@ use Moo;
 use Dancer::Plugin;
 use Articulate::Service;
 
+=head1 NAME
+
+Articulate::Request - represent a request
+
+=cut
+
+=head1 FUNCTIONS
+
+=head3 articulate_request
+
+  my $request = articulate_request verb => $data;
+
+Creates a new request, using the verb and data supplied as the respective arguments.
+
+=cut
+
 register articulate_request => sub {
   __PACKAGE__->new( {
      verb => shift,
@@ -11,17 +27,47 @@ register articulate_request => sub {
   } );
 };
 
-has verb =>
-  is      => 'rw',
-  default => sub { 'error' };
+=head1 METHODS
 
-has data =>
-  is      => 'rw',
-  default => sub { { } };
+=head3 new
+
+An unremarkable Moo constructor.
+
+=cut
+
+=head3 perform
+
+Sends the to the articulate service.
+
+Note: the behaviour of this method may change!
+
+=cut
 
 sub perform {
   service->process_request(shift);
 }
+
+=head1 ATTRIBUTES
+
+=head3 verb
+
+The action being performed, e.g. C<create>, C<read>, etc. The verbs available are entirely dependant on the application: A request will be handled by a service provider (see Articulate::Service) which will typically decide if it can fulfil the request based on the verb.
+
+=cut
+
+has verb =>
+  is      => 'rw',
+  default => sub { 'error' };
+
+=head3 data
+
+The information passed along with the request, e.g. C<< { location => '/zone/public/article/hello-world' } >>. This should always be a hashref.
+
+=cut
+
+has data =>
+  is      => 'rw',
+  default => sub { { } };
 
 register_plugin();
 
--- lib/Articulate/Response.pm
+++ lib/Articulate/Response.pm
@@ -4,6 +4,24 @@ use Moo;
 use Dancer::Plugin;
 use Articulate::Serialisation ();
 
+
+=head1 NAME
+
+Articulate::Response - represent a response
+
+=cut
+
+=head1 FUNCTIONS
+
+=head3 response
+=head3 articulate_response
+
+  my $response = response type => $data;
+
+Creates a new response, using the type and data supplied as the respective arguments. Using this constructor, the error code will be 200 unless C<type> is C<error>.
+
+=cut
+
 register response => sub {
   __PACKAGE__->new( {
     type => $_[0],
@@ -12,22 +30,71 @@ register response => sub {
   } );
 };
 
+=head1 METHODS
+
+=head3 new
+
+An unremarkable Moo constructor.
+
+=cut
+
+=head3 serialise
+
+Sends the response to Articulate::Serialisation->serialise.
+
+Note: the behaviour of this method may change!
+
+=cut
+
+sub serialise { # this is convenient as it is probably the next thing which will always be done.
+  Articulate::Serialisation::serialisation->serialise (shift);
+}
+
+
+=head1 ATTRIBUTES
+
+=head3 http_code
+
+The HTTP response code which best applies to the response. The default is 500.
+
+It is not guaranteed that this will be passed to the ultimate client (e.g. a later error may cause a 500; the service may be accessed in a way other than HTTP).
+
+=cut
+
 has http_code =>
   is      => 'rw',
   default => 500,
   coerce  => sub { 0+shift };
 
+
+=head3 type
+
+The type of response, which will be used by serialisers etc. to determine how to complete processing (e.g. which template to use).
+
+=cut
+
 has type =>
   is      => 'rw',
   default => sub { 'error' };
 
+=head3 data
+
+The actual content of the response, including any metadata. Typically this will be of the form
+
+  {
+    item => {
+      article => {
+        meta    => { ... }
+        content => " ... "
+      }
+    }
+  }
+
+=cut
+
 has data =>
   is      => 'rw',
   default => sub { { } };
-
-sub serialise { # this is convenient as it is probably the next thing which will always be done.
-  Articulate::Serialisation::serialisation->serialise (shift);
-}
 
 register_plugin();
 
