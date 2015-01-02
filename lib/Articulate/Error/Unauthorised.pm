3f225ef830b00833b85f4b68736e1d020e1919c6
Daniel Perrett <perrettdl@googlemail.com>
2015-01-02 21:58:23 +0000

    Documented error classes

--- lib/Articulate/Error/Unauthorised.pm
+++ lib/Articulate/Error/Unauthorised.pm
@@ -3,6 +3,26 @@ package Articulate::Error::Unauthorised;
 use Moo;
 extends 'Articulate::Error';
 
+=head1 NAME
+
+Articulate::Error::Unauthorised - represent an error indicating the authorisation header field is missing.
+
+=cut
+
+=head3 DESCRIPTION
+
+This class extends Articulate::Error and merely sets two default values:
+
+=head3 simple_message
+
+This defaults to C<'Unauthorised'>.
+
+=head3 http_code
+
+This defaults to C<401>.
+
+=cut
+
 has '+simple_message' =>
   default => 'Unauthorised';
 
