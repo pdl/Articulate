6b9dbf24e95e177493ff9b26b1d49ecb82ae1740
Daniel Perrett <perrettdl@googlemail.com>
2015-01-09 08:42:47 +0000

    Tidied up some typos in Articulate.pm docs; added section 'Getting started'

--- lib/Articulate.pm
+++ lib/Articulate.pm
@@ -15,7 +15,7 @@ Articulate - A lightweight Perl CMS Framework
 	articulate_app->enable;
 	dance;
 
-Articulate provides a content management service for your web app. Lightweight means placing minimal demands on your app while maximising 'whipuptitude': it gives you a single interface in code to a framework that's totally modular underneath, and it won't claim any URL endpoints for itself.
+Articulate provides a content management service for your web app. It's lightweight, i.e. it places minimal demands on your app while maximising 'whipuptitude': it gives you a single interface in code to a framework that's totally modular underneath, and it won't claim any URL endpoints for itself.
 
 You don't need to redesign your app around Articulate, it's a service that you call on when you need it, and all the 'moving parts' can be switched out if you want to do things your way.
 
@@ -23,13 +23,13 @@ It's written in Perl, the fast, reliable 'glue language' that's perfect for agil
 
 =head1 DESCRIPTION
 
-Articulate is (currently) a set of Dancer plugins which work together to create a conent management service that will sit alongside an existing Dancer app or for the basis of a new one.
+Articulate is (currently) a set of Dancer plugins which work together to create a conent management service that will sit alongside an existing Dancer app or form the basis of a new one.
 
 =head2 Response/Request lifecycle summary
 
 In a B<route>, you parse user input and pick the parameters you want to send to the service. Have a look at L<Articulate::Routes::Transparent> for some examples. The intention is that routes are as 'thin' as possible: business logic should all be done by some part of the service and not in the route handler. The route handler maps endpoints (URLs) to service requests and serialises the structured response.
 
-A B<request> contains a B<verb> (like C<create>) and B<data> (like the B<location> you want to create it at and the B<content> you want to place there). See L<Articulate::Request> for more details.
+B<Routes> pass B<requests> to B<services>. A B<request> contains a B<verb> (like C<create>) and B<data> (like the B<location> you want to create it at and the B<content> you want to place there). See L<Articulate::Request> for more details.
 
 The B<service> is responsible for handling requests for managed content. L<Articulate::Service> B<delegates> to a service B<provider>, asking each in turn if they are willing to handle the request (normally the provider will dertermine this based on the request verb). A provider typically checks a user has suitable permission, then interacts with the content storage system.
 
@@ -37,13 +37,13 @@ B<Storage> is controlled by L<Articulate::Storage>. It delegates to a storage cl
 
 Content is stored in a structure called an B<item> (see L<Articulate::Item>), which has a C<location> (see L<Articulate::Location>), the B<content> (which could be a binary blob like an image, plain text, markdown, XML, etc.) and the associated B<metadata> or B<meta> (which is a hashref).
 
-Before items can be placed in storage, the service should take care to run them through B<validation>. C<Articulate::Validation> delegates this to validators, and if there are any applicable validators, they will check if the content is valid.
+Before items can be placed in storage, the service should take care to run them through B<validation>. C<Articulate::Validation> delegates this to validators, and if there are any applicable validators, they will check if the content is valid. The content may also be B<enriched>, i.e. further metadata added, like the time at which the request was made.
 
 If at any time uncaught errors are thrown, including recognised C<Articulate::Error> objects, they are caught and handled by C<Articulate::Service>. C<Articulate::Service> should therefore always return an C<Articulate::Response> object.
 
 Once the request finds it back to the Route it will typically be B<serialised> immediately (see L<Articulate::Serialiser>), and the resulting response passed back to the user.
 
-Finally, after items are retrieved from storage, there is the opportunity to C<augment> them, for instance by including relevant content from elsewhere which belongs in the response. See L<Articulate::Augmentation> for details on this.
+Finally, after items are retrieved from storage, there is the opportunity to B<augment> them, for instance by including relevant content from elsewhere which belongs in the response. See L<Articulate::Augmentation> for details on this.
 
 =head2 Components
 
@@ -54,6 +54,10 @@ The following classes are persistent, configurable components of the system
 =item * L<Articulate::Augmentation>
 
 =item * L<Articulate::Authentication>
+
+=item * L<Articulate::Construction>
+
+=item * L<Articulate::Enrichment>
 
 =item * L<Articulate::Intepreter>
 
@@ -79,11 +83,19 @@ The following classes are used for passing request data between components:
 
 =item * L<Articulate::Location>
 
+=item * L<Articulate::Permission>
+
 =item * L<Articulate::Request>
 
 =item * L<Articulate::Response>
 
 =back
+
+=head1 GETTING STARTED
+
+Check out the examples in the C<examples> folder of the distribution.
+
+To add Articulate to your own app, you'll need to add it to your C<bin/app.pl> or similar: C<use Articulate> then C<articulate_app->enable>, and you'll need to add routes and services in your C<config.yml>. Check each of the components for a description of their config options. Or just borrow a config from one of the examples.
 
 =head1 BUGS
 
