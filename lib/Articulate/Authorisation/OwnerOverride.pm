deb9fe9f2f1ab11381df241f41f525b2c6efb8e1
Daniel Perrett <perrettdl@googlemail.com>
2015-01-10 20:52:53 +0000

    Documented Authorisation providers (and discovered a semicolon way off to the right of the screen)

--- lib/Articulate/Authorisation/OwnerOverride.pm
+++ lib/Articulate/Authorisation/OwnerOverride.pm
@@ -3,10 +3,58 @@ package Articulate::Authorisation::OwnerOverride;
 use Moo;
 with 'MooX::Singleton';
 
+=head1 NAME
+
+Articulate::Authorisation::OwnerOverride - always say yes to the owner
+
+=cut
+
+=head1 CONFIGURATION
+
+Put this in your config:
+
+  plugins:
+    Articulate::Authorisation:
+      providers:
+        - Articulate::Authorisation::OwnerOverride
+
+Or, if you want the owner
+
+  plugins:
+    Articulate::Authorisation:
+      providers:
+        - class: Articulate::Authorisation::OwnerOverride
+          args:
+            owner: administrator
+
+=head1 ATTRIBUTES
+
+=head3 owner
+
+The username of the owner. Defaults to C<owner>.
+
+=cut
+
+has owner =>
+  is      => 'rw',
+  default => sub{'owner'};
+
+=head1 METHODS
+
+=head3 new
+
+Yep, C<new> works just as you'd expect.
+
+=head3 permitted
+
+Grants any request if the user asking is the owner. By default this is the user called C<owner>, but it could be someone else, if the C<owner> attribute is set.
+
+=cut
+
 sub permitted {
   my $self       = shift;
   my $permission = shift;
-  $permission->grant('Site owner can do anything') if ( ( $permission->user // '' ) eq 'owner' ); 
+  $permission->grant('Site owner can do anything') if ( ( $permission->user // '' ) eq $self->owner );
   return $permission;
 }
 
--- lib/Articulate/Authorisation/Preconfigured.pm
+++ lib/Articulate/Authorisation/Preconfigured.pm
@@ -5,9 +5,53 @@ use warnings;
 use Moo;
 with 'MooX::Singleton';
 
+=head1 NAME
+
+Articulate::Authorisation::Preconfigured - allow access to users in your config
+
+=cut
+
+=head1 CONFIGURATION
+
+Put this in your config:
+
+  plugins:
+    Articulate::Authorisation:
+      providers:
+        - class: Articulate::Authorisation::Preconfigured
+          rules:
+            zone/public:
+              "[guest]":
+                read: 1
+              admin: 1
+
+=head1 ATTRIBUTES
+
+=head3 rules
+
+The rules used to determine whether or not requests are authorised. Defaults to C<{}>.
+
+=cut
+
 has rules =>
   is      => 'rw',
   default => sub { {} };
+
+=head1 METHODS
+
+=head3 new
+
+No surprises here.
+
+=head3 permitted
+
+Goes through each of the locations in 'rules' (in ascending order of length) and if the location in the permission request begins with that rule, then look at the contents.
+
+We then expect a hash of user ids, or C<[guest]> for users not logged in. Their values should be 0 (for deny), 1 (for grant), or a hash of verbs to grant/deny.
+
+This is preconfigured access, so fine for a small personal or static site, but if you have open sign-up or changing requirements then you will probably find changing the config file and reloading the app gets tedious after a while.
+
+=cut
 
 sub permitted {
   my $self       = shift;
@@ -30,7 +74,7 @@ sub permitted {
         }
         else {
           my $value = !! $rules->{$rule_location}->{$user_id};
-          return $permission->deny("User cannot access $rule_location at all") unless $value                                                                                                                    ;
+          return $permission->deny("User cannot access $rule_location at all") unless $value;
           $access = "User can access $rule_location";
         }
       }
