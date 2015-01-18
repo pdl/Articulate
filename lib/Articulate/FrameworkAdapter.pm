c777bcf1d4518c7c977ee031026998c26fa89cfa
Daniel Perrett <perrettdl@googlemail.com>
2015-01-18 18:33:53 +0000

    FrameworkAdapter now delegates

--- lib/Articulate/FrameworkAdapter.pm
+++ lib/Articulate/FrameworkAdapter.pm
@@ -2,21 +2,20 @@ package Articulate::FrameworkAdapter;
 
 use Moo;
 with 'MooX::Singleton';
-use Dancer qw(:syntax !after !before);
+use Articulate::Syntax qw(instantiate);
 use Dancer::Plugin;
 
 # Currently, only Dancer1 is supported.
-# Later, we will do delegation. For now, we collect the important communication with the framework in anticipation.
 
 register framework => sub {
-  __PACKAGE__->instance(plugin_setting);
+  __PACKAGE__->instance(plugin_setting)->provider;
 };
 
-
-sub user {
-  my $self = shift;
-  session ('user_id', @_);
-}
+has provider => (
+  is      => 'rw',
+  default => sub { 'Articulate::FrameworkAdapter::Dancer1' },
+  coerce  => sub { instantiate(@_) },
+);
 
 register_plugin();
 
---
+++ lib/Articulate/FrameworkAdapter/Dancer1.pm
@@ -0,0 +1,17 @@
+package Articulate::FrameworkAdapter::Dancer1;
+
+use Moo;
+with 'MooX::Singleton';
+use Dancer qw(:syntax !after !before);
+
+sub user_id {
+  my $self = shift;
+  session ('user_id', @_);
+}
+
+sub user {
+  my $self = shift;
+  session ('user_id', @_);
+}
+
+1;
