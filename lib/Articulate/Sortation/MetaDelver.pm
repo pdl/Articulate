947ba6d79c8db7367d3b99cf1f0a7e14b5592b0a
Daniel Perrett <perrettdl@googlemail.com>
2015-01-30 08:32:27 +0000

    Some docs for Sortation::MetaDelver

--- lib/Articulate/Sortation/MetaDelver.pm
+++ lib/Articulate/Sortation/MetaDelver.pm
@@ -12,9 +12,31 @@ use Dancer::Plugin;
 
 Articulate::Sortation::MetaDelver - delve into the metadata and sort it
 
+=head1 ATTRIBUTES
+
+=head3 options
+
+A hashref defining what information to get and how to sort it.
+
+=over
+
+=item * C<field>: . This field is mandatory. A C<Data::DPath> expression used to find the right field from the meta, e.g. 'schema/core/dateUpdated' Not that a '/' will be prepended if it does not already exist..
+
+=item * C<cmp> - The type of comparison to apply - any class which provides a C<cmp> method can be entered here. Defaults to C<Articulate::Sortation::String>.
+
+=item * C<order> - The direction in which to sort: C<asc> or C<desc>, defaults to C<asc>.
+
+=back
+
 =head1 METHODS
 
 =head3 sort
+
+Provided by L<Articulate::Role::Sortation::AllYouNeedIsCmp>.
+
+=head3 schwartz
+
+Provided by L<Articulate::Role::Sortation::AllYouNeedIsCmp>.
 
 =cut
 
