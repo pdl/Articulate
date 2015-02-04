2464b7dfe02d038eaedec6f8ea3b5fd033a1bbb7
Daniel Perrett <perrettdl@googlemail.com>
2015-02-04 08:33:39 +0000

    Documented D1 and D2 plugins

--- lib/Dancer/Plugin/Articulate.pm
+++ lib/Dancer/Plugin/Articulate.pm
@@ -3,6 +3,43 @@ package Dancer::Plugin::Articulate;
 use Dancer::Plugin;
 use Articulate;
 
+=head1 NAME
+
+Dancer::Plugin::Articulate - use Articulate in your Dancer App
+
+=head1 SYNOPSIS
+
+	use Dancer;
+	use Dancer::Plugin::Articulate;
+	my $app = articulate_app;
+	$app->enable;
+	dance;
+
+	# in config.yml
+	plugins:
+		Articulate:
+			framework: Articulate::FrameworkAdapter::Dancer1
+			# Other Articulate config goes here
+
+Creates an instance of L<Articulate> using your Dancer config, and enables the app, declaring routes, etc.
+See L<Articulate> for how to configure and use it, and L<Articulate::FrameworkAdapter::Dancer1> for details of the integration between Dancer1 and Articulate.
+
+=head1 SEE ALSO
+
+=over
+
+=item * L<Dancer2::Plugin::Articulate>
+
+=item * L<Dancer::Plugins>
+
+=item * L<Dancer::Config>
+
+=item * L<Articulate::FrameworkAdapter::Dancer1>
+
+=back
+
+=cut
+
 register articulate_app => sub {
 	return Articulate->instance(plugin_setting);
 };
--- lib/Dancer2/Plugin/Articulate.pm
+++ lib/Dancer2/Plugin/Articulate.pm
@@ -5,6 +5,39 @@ use warnings;
 use Dancer2::Plugin;
 use Articulate;
 
+=head1 NAME
+
+Dancer2::Plugin::Articulate - use Articulate in your Dancer2 App
+
+=head1 SYNOPSIS
+
+	# in config.yml
+	plugins:
+		Articulate:
+			framework:
+				Articulate::FrameworkAdapter::Dancer2:
+					appname: MyApp
+			# Other Articulate config goes here
+
+Creates an instance of L<Articulate> using your Dancer2 config, and enables the app, declaring routes, etc.
+See L<Articulate> for how to configure and use it, and L<Articulate::FrameworkAdapter::Dancer2> for details of the integration between Dancer2 and Articulate.
+
+=head1 SEE ALSO
+
+=over
+
+=item * L<Dancer::Plugin::Articulate>
+
+=item * L<Dancer2::Plugins>
+
+=item * L<Dancer2::Config>
+
+=item * L<Articulate::FrameworkAdapter::Dancer2>
+
+=back
+
+=cut
+
 register articulate_app => sub {
 	my $dsl = shift;
 	Articulate->instance (plugin_setting);
