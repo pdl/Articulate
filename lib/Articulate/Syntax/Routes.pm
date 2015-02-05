bc41c05ed86dc71d97dbae12ce4d7f2f119a831c
Daniel Perrett <perrettdl@googlemail.com>
2015-02-05 08:35:15 +0000

    Refactored _declare_route in Syntax::Routes

--- lib/Articulate/Syntax/Routes.pm
+++ lib/Articulate/Syntax/Routes.pm
@@ -13,9 +13,9 @@ default_exports qw(
 
 no warnings 'redefine';
 
-sub on_enable(&) {
+sub on_enable {
   my $code = shift;
-  my ($pkg) = caller(1);
+  my ($pkg) = caller(2);
   my $routes = "${pkg}::__routes";
   {
     no strict 'refs';
@@ -24,56 +24,23 @@ sub on_enable(&) {
   }
 }
 
-sub get {
-  my $path = shift;
-  my $code  = pop;
-  on_enable {
+sub _declare_route {
+  my ( $http_verb, $path, $code ) = @_;
+  on_enable ( sub {
     my $self = shift;
     my $wrapped = sub{ $self->serialisation->serialise( $code->( @_ ) ) };
-    $self->framework->declare_route( get => $path => sub { perform_request( $wrapped, [ $self, request ] ) } );
-  }
+    $self->framework->declare_route( $http_verb => $path => sub { perform_request( $wrapped, [ $self, request ] ) } );
+  } );
 }
 
+sub any     { _declare_route ( any     => @_ ); }
+sub get     { _declare_route ( get     => @_ ); }
+sub post    { _declare_route ( post    => @_ ); }
+sub patch   { _declare_route ( patch   => @_ ); }
+sub del     { _declare_route ( del     => @_ ); }
+sub put     { _declare_route ( put     => @_ ); }
+sub options { _declare_route ( options => @_ ); }
 
-sub post {
-  my $path = shift;
-  my $code  = shift;
-  on_enable {
-    my $self = shift;
-    my $wrapped = sub{ $self->serialisation->serialise( $code->( @_ ) ) };
-    $self->framework->declare_route( post => $path => sub { perform_request( $wrapped, [ $self, request ] ) } );
-  }
-}
-
-sub patch {
-  my $path = shift;
-  my $code  = shift;
-  on_enable {
-    my $self = shift;
-    my $wrapped = sub{ $self->serialisation->serialise( $code->( @_ ) ) };
-    $self->framework->declare_route( patch => $path => sub { perform_request( $wrapped, [ $self, request ] ) } );
-  }
-}
-
-sub del {
-  my $path = shift;
-  my $code  = shift;
-  on_enable {
-    my $self = shift;
-    my $wrapped = sub{ $self->serialisation->serialise( $code->( @_ ) ) };
-    $self->framework->declare_route( del => $path => sub { perform_request( $wrapped, [ $self, request ] ) } );
-  }
-}
-
-sub put {
-  my $path = shift;
-  my $code  = shift;
-  on_enable {
-    my $self = shift;
-    my $wrapped = sub{ $self->serialisation->serialise( $code->( @_ ) ) };
-    $self->framework->declare_route( put => $path => sub { perform_request( $wrapped , [ $self, request ] ) } );
-  }
-}
 
 sub perform_request {
   $_[0]->(@{ $_[1] });
