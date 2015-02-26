7116b30480024400e5df39152572355de0e16554
Daniel Perrett <perrettdl@googlemail.com>
2015-02-26 09:00:10 +0000

    Documented Articulate::Syntax

--- lib/Articulate/Syntax.pm
+++ lib/Articulate/Syntax.pm
@@ -35,11 +35,48 @@ use Articulate::Response;
 # sub throw_error { Articulate::Error::throw_error(@_) };
 # sub loc         { Articulate::Location::loc(@_) };
 
+=head1 NAME
+
+Articulate::Syntax - Common functions and syntactic sugar for
+Articulate
+
+=head1 FUNCTIONS
+
+=head3 response
+
+See L<Artciulate::Response>.
+
+=head3 articulate_request
+
+See L<Artciulate::Request>.
+
+=head3 credentials
+
+See L<Artciulate::Credentials>.
+
+=head3 permission
+
+See L<Artciulate::Permission>.
+
+=head3 loc
+
+See L<Artciulate::Location>.
+
+=head3 locspec
+
+See L<Artciulate::LocationSpecification>.
+
+=cut
+
 =head3 instantiate_array
 
-C<instantiate_array> accepts an arrayref of values which represent objects. For each value, if it is not an object, it will attempt to instantiate one using C<instantiate>.
+C<instantiate_array> accepts an arrayref of values which represent
+objects. For each value, if it is not an object, it will attempt to
+instantiate one using C<instantiate>.
 
-If you pass C<instantiate_array> a value which is not an arrayref, it will assume you meant to give it an arrayref with a single item; or, if you pass it C<undef>, it will return an empty arrayref.
+If you pass C<instantiate_array> a value which is not an arrayref, it
+will assume you meant to give it an arrayref with a single item; or, if
+you pass it C<undef>, it will return an empty arrayref.
 
 The purpose of this function is to enable the following:
 
@@ -63,15 +100,24 @@ Which means given config like the following:
           constructor: preset
           args: pdf
 
-You can be guaranteed that looping through C<< @{ $self->delegates_to } >> will always produce objects.
+You can be guaranteed that looping through C<< @{ $self->delegates_to }
+>> will always produce objects.
 
 =head3 instantiate
 
 Attempts to create an object from the hashref or class name provided.
 
-If the value is a string, it will treat as a class name, and perform C<< $class->new >>, or, if the method exists, C<< $class->instance >> will be preferred (for instance, as provided by C<MooX::Singleton>).
+If the value is a string, it will treat as a class name, and perform
+C<< $class->new >>, or, if the method exists, C<< $class->instance >>
+will be preferred (for instance, as provided by C<MooX::Singleton>).
 
-If the value is a hashref, it will look at the values for the keys C<class>, C<constructor>, and C<args>. It will then attempt to perform C<< $class->$constructor(@$args) >>, unless the constructor is absent (in which case C<instance> or C<new> will be supplied), or if C<args> is not an arrayref, in which case it will be passed to the constructor as a single argument (or the empty list will be passed if C<args> is undefined).
+If the value is a hashref, it will look at the values for the keys
+C<class>, C<constructor>, and C<args>. It will then attempt to perform
+C<< $class->$constructor(@$args) >>, unless the constructor is absent
+(in which case C<instance> or C<new> will be supplied), or if C<args>
+is not an arrayref, in which case it will be passed to the constructor
+as a single argument (or the empty list will be passed if C<args> is
+undefined).
 
 If the value is an object, the object will simply be returned.
 
@@ -129,7 +175,8 @@ sub instantiate_array {
 
   sub email_address { from_meta (shift, 'schema/user/email_address'); }
 
-This method uses Data::DPath to retrieve a field from the metadata structure.
+This method uses Data::DPath to retrieve a field from the metadata
+structure.
 
 =cut
 
@@ -184,9 +231,12 @@ sub dpath_set {
     default => sub { {} },
     coerce  => sub { instantiate_selection @_ };
 
-Expects a hash. If any of the values are single-key hashes with the key 'alias' then the alias is resolved. Otherwise the value is instantiated.
+Expects a hash. If any of the values are single-key hashes with the key
+'alias' then the alias is resolved. Otherwise the value is
+instantiated.
 
-If a value other than a hash is given, returns a hash with the key 'default' and the original value instantiated.
+If a value other than a hash is given, returns a hash with the key
+'default' and the original value instantiated.
 
 =cut
 
@@ -209,9 +259,12 @@ sub instantiate_selection {
     default => sub { {} },
     coerce  => sub { instantiate_array_selection @_ };
 
-Expects a hash. If any of the values are single-key hashes with the key 'alias' then the alias is resolved. Otherwise the value is instantiated as an array (see C<instantiate_aray>).
+Expects a hash. If any of the values are single-key hashes with the key
+'alias' then the alias is resolved. Otherwise the value is instantiated
+as an array (see C<instantiate_aray>).
 
-If a value other than a hash is given, returns a hash with the key 'default' and the original value instantiated as an array.
+If a value other than a hash is given, returns a hash with the key
+'default' and the original value instantiated as an array.
 
 =cut
 
@@ -261,7 +314,8 @@ sub _instantiate_selection {
   $self->schema('schema_generic'); # same thing, because of alias
   $self->schemata->{default}; # This is what they all do in practice
 
-Implements a user-friendly selection mechanism like the one implemented by C<Dancer::Plugin::DBIC::schema>.
+Implements a user-friendly selection mechanism like the one implemented
+by C<Dancer::Plugin::DBIC::schema>.
 
 =cut
 
@@ -286,7 +340,9 @@ sub select_from {
   is_single_key_hash ( { foo => 123 }, 'foo' ); # returns 1
   is_single_key_hash ( { foo => 123 }, 'bar' ); # returns 0
 
-Returns 1 if the first argument is a hashref with exactly one key. If a second argument is provided, then the key, if it exists, must be equal to that argument, or the return value will be 0.
+Returns 1 if the first argument is a hashref with exactly one key. If a
+second argument is provided, then the key, if it exists, must be equal
+to that argument, or the return value will be 0.
 
 =cut
 
@@ -307,7 +363,10 @@ sub is_single_key_hash {
 
   my $merged = hash_merge ($parent, $child)
 
-Returns a new hashref whose values represent a union of the parent's and the child's. The child's values overwrite the parent, in case of conflict. The merge is deep (i.e. it handles nested hashes), using L<Hash::Merge> with right precedence.
+Returns a new hashref whose values represent a union of the parent's
+and the child's. The child's values overwrite the parent, in case of
+conflict. The merge is deep (i.e. it handles nested hashes), using
+L<Hash::Merge> with right precedence.
 
 =cut
 
