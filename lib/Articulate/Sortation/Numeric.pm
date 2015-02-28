6177b00ed735d359f193622fb3c987d0251a9c4e
Daniel Perrett <perrettdl@googlemail.com>
2015-02-28 18:58:07 +0000

    Some documentation for Sortation providers

--- lib/Articulate/Sortation/Numeric.pm
+++ lib/Articulate/Sortation/Numeric.pm
@@ -5,9 +5,42 @@ use warnings;
 use Moo;
 with 'Articulate::Role::Sortation::AllYouNeedIsCmp';
 
+=head1 NAME
+
+Articulate::Sortation::Numeric - sort strings using the spaceship
+operator
+
+=head1 DESCRIPTION
+
+This implements the L<Articulate::Role::Sortation::AllYouNeedIsCmp>
+role to provide a very basic numerical sorter object.
+
+=head1 METHODS
+
+One method provided here, the rest are as in
+L<Articulate::Role::Sortation::AllYouNeedIsCmp>.
+
+=head3 cmp
+
+  $self->cmp($a, $b); # returns $a <=> $b
+
+=cut
+
 sub cmp {
   my $self = shift;
   return ( $_[0] <=> $_[1] );
 }
+
+=head1 SEE ALSO
+
+=over
+
+=item * L<Articulate::Sortation::String>
+
+=item * L<Articulate::Sortation::Slug>
+
+=back
+
+=cut
 
 1;
--- lib/Articulate/Sortation/Slug.pm
+++ lib/Articulate/Sortation/Slug.pm
@@ -5,6 +5,33 @@ use warnings;
 use Moo;
 with 'Articulate::Role::Sortation::AllYouNeedIsCmp';
 
+=head1 NAME
+
+Articulate::Sortation::Slug - natural slug sorting
+
+=head1 DESCRIPTION
+
+This implements the L<Articulate::Role::Sortation::AllYouNeedIsCmp>
+role to provide a sorter object which will break up
+
+=head1 METHODS
+
+One method provided here, the rest are as in
+L<Articulate::Role::Sortation::AllYouNeedIsCmp>.
+
+=head3 cmp
+
+  $self->cmp('foo-1', 'bar');    # bar comes before foo
+  $self->cmp('foo-2', 'foo-11'); # 2 comes before 11
+  $self->cmp('foo-2',  'food');  # foo comes before food
+
+Splits into numerical and alphabetical. Sorts the former numerically
+and the latter alphabetically. All characters other than a-z and 0-9
+are treated as 'word breaks' and divide up components but are otherwise
+ignored.
+
+=cut
+
 sub _left { -1 }
 
 sub _right { 1 }
@@ -63,5 +90,17 @@ sub cmp {
   die 'shouldn\'t be here' if $left ne $right;
   return $left cmp $right;
 }
+
+=head1 SEE ALSO
+
+=over
+
+=item * L<Articulate::Sortation::String>
+
+=item * L<Articulate::Sortation::Numeric>
+
+=back
+
+=cut
 
 1;
