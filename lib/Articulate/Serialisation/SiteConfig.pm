cf941f979d3ec3b0ed5218034cbd81fcd97a0755
Daniel Perrett <perrettdl@googlemail.com>
2015-03-06 08:21:00 +0000

    More documentation for Serialisation::SiteConfig

--- lib/Articulate/Serialisation/SiteConfig.pm
+++ lib/Articulate/Serialisation/SiteConfig.pm
@@ -7,13 +7,36 @@ with 'Articulate::Role::Component';
 
 =head1 NAME
 
-PlainSpeaking::Serialisation::SiteConfig
+PlainSpeaking::Serialisation::SiteConfig - add some site-wide constants
+to your response data
 
-=head1 METHODS
+=head1 SYNOPSIS
+
+  components:
+    serialisation:
+      - Articulate::Serialisation::SiteData:
+          site_data:
+            # anything you want can go here
+            title: My lovely website
+            css:
+              - base.css
+              - print.css
+            js:
+              - jquery.min.js
+              - jquery.ui.min.js
+      - Articulate::Serialisation::TemplateToolkit
+
+=head1 METHOD
 
 =head3 serialise
 
-Adds site data to the response data.
+Adds site data to the response data. Returns undef.
+
+=head1 ATTRIBUTE
+
+=head1 site_data
+
+A hashref which will be added to C<< @{ $response->data->{site} } >>.
 
 =cut
 
